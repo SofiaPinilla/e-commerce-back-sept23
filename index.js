4046ed42619adb417ac32b550c4e0813004818a1
SofiaPinilla <contact@sofiapinilla.com>
2023-11-13 12:14:07 +0100

    authentication implemented

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const PORT = 8080;
 const { dbConnection } = require("./config/config");
 
 app.use(express.json());
-app.use("/products",require("./routes/products"))
+
+app.use("/products", require("./routes/products"));
+app.use("/users", require("./routes/users"));
+app.use("/orders",require("./routes/orders"))
 
 dbConnection();
 
--- routes/products.js
+++ routes/products.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const ProductController = require("../controllers/ProductController");
+const { authentication, isAdmin } = require("../middleware/authentication");
 const router = express.Router();
 
-router.post("/", ProductController.create);
+router.post("/", authentication,isAdmin, ProductController.create);
 router.get("/", ProductController.getAll);
 router.get("/id/:_id", ProductController.getById);
 router.get("/name/:name",ProductController.getProductsByName)
-router.delete("/id/:_id",ProductController.delete)
-router.put("/id/:_id",ProductController.update)
+router.delete("/id/:_id",authentication,ProductController.delete)
+router.put("/id/:_id",authentication,ProductController.update)
 
 module.exports = router;
